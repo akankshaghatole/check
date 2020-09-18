c6d0972ea0d46cc06a5e546b81b209489a328bdc
akankshaghatole <63338787+akankshaghatole@users.noreply.github.com>
2020-09-17 22:40:22 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
     <head>
-        <title>Dinne In Restaurant_Menu</title>
+        <title>Health Analysis</title>
         
 		<link rel="stylesheet" type="text/css" href="style.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
@@ -15,49 +15,49 @@
     <nav class="navbar navbar-dark navbar-expand-sm fixed-top">
       <div class="collapse navbar-collapse" id="Navbar">
         <ul class="navbar-nav mr-auto">
-          <li id="btn1" class="nav-item active"><a class="nav-link" href="#"><span class="brandname">Dinne In</span> Menu is Here</a></li>
+          <li id="btn1" class="nav-item active"><a class="nav-link" href="#"><span class="brandname">Health Analysis</span>Know more about disease</a></li>
         </ul>
       </div>
     </nav>
     <div class="head">
-      <h1>Welcome to <span class="title">Dinne In !!</span></h1>
+      <h1>Analysis <span class="title">Yourself!!</span></h1>
     </div>
     <div class="container">
       <select name="item restaurant-dropdown restaurant" class="custom-select custom-select-lg mb-3" id="restaurant">
-        <option value="">Disease List </option>
+        <option value="">Disease List</option>
       </select>
       <br>    
       <table id="table" class="table table-light table table-hover table-bordered table_data">
       <tr>
-        <th>Name</th>
+        <th>Common Name </th>
         <td id="menuname"></td>
       </tr>
       <tr>
-        <th>ID</th>
+        <th>Mostcommon Symptoms </th>
         <td id="id"></td>
       </tr>
       <tr>
-        <th>Scientific Name </th>
+        <th>Scientific Name</th>
         <td id="sname"></td>
       </tr>
       <tr>
-        <th>Symptoms</th>
+        <th>Serious Symptoms</th>
         <td id="descp"></td>
       </tr>
       <tr>
-        <th>Prevention</th>
+        <th>When to visit Doctor</th>
         <td id="psmall"></td>
       </tr>
       <tr>
-        <th>When to visit doctor </th>
+        <th>Testing required</th>
         <td id="plarge"></td>
       </tr>
       <tr>
-        <th>Small Portion Name</th>
+        <th>Prevention</th>
         <td id="spname"></td>
       </tr>
       <tr>
-        <th>Large Portion Name</th>
+        <th>Other Info</th>
         <td id="lpname"></td>
       </tr>
       </table>
